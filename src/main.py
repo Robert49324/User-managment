678b3ea01c21e272e3775053637157b97328fed3
JustPC <robert49324@gmail.com>
2024-02-05 10:37:46 +0300

    logger works

--- src/main.py
+++ src/main.py
@@ -3,18 +3,14 @@ import logging.config
 import uvicorn
 from fastapi import FastAPI
 
-logging.config.fileConfig('logging.ini')
-logger = logging.getLogger('User_managment')
+logging.config.fileConfig('./logging.ini')
+logger = logging.getLogger('user_managment')
 
 app = FastAPI()
 
 @app.get("/")
 async def root():
-    logger.debug("debug message", extra={"x": "hello"})
-    logger.info("info message")
-    logger.warning("warning message")
-    logger.error("error message")
-    logger.critical("critical message")
+    logger.debug("Root endpoint called")
     return {"message": "check"}
 
 if __name__ == "__main__":
