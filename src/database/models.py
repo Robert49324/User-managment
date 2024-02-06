b84bcc3a62213e77c675a0c2c8679fd1aa18c1d1
JustPC <robert49324@gmail.com>
2024-02-06 14:54:58 +0300

    added alembic migrations

--- src/database/models.py
+++ src/database/models.py
@@ -1,8 +1,8 @@
-from sqlalchemy import Column, Integer, DateTime, Boolean, Enum, ForeignKey, Text
+from sqlalchemy import Column, Integer, DateTime, Boolean, Enum, ForeignKey, Text, UUID
 from sqlalchemy.sql import func
 from sqlalchemy.orm import mapped_column
-from database import Base
-import datetime
+from .database import Base
+import uuid
 
 class Role(Enum):
     USER = "USER"
@@ -12,14 +12,14 @@ class Role(Enum):
 class User(Base):
     __tablename__ = "users"
     
-    id = mapped_column(Text,primary_key=True)
+    id = mapped_column(UUID,primary_key=True, default=uuid.uuid4)
     name = mapped_column(Text, nullable=False)
     surname = mapped_column(Text, nullable=False)
     username = mapped_column(Text, nullable=False)
     password = mapped_column(Text,nullable=False)
     phone_number = mapped_column(Text, unique=True)
     email = mapped_column(Text, unique=True, nullable=False)
-    role = mapped_column(Role)
+    role = mapped_column(Role, nullable=False, default=Role.USER)
     group = mapped_column(ForeignKey("group.id"))
     image = mapped_column(Text, unique=True)
     is_blocked = mapped_column(Boolean)
