6410d602a73848b97742ed786df47a29609fee0e
JustPC <robert49324@gmail.com>
2024-03-04 20:06:54 +0300

    ci/cd

--- migration/env.py
+++ migration/env.py
@@ -5,12 +5,14 @@ from alembic import context
 from sqlalchemy import pool
 from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
+from src.config import Settings
 
 from src.models import Base
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+config.set_main_option("sqlalchemy.url", Settings.postgres_url)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
