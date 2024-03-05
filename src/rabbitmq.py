df17040a00331deca71b54b95b28f23d9a234e74
JustPC <robert49324@gmail.com>
2024-03-05 19:30:30 +0300

    ci/cd

--- src/rabbitmq.py
+++ src/rabbitmq.py
@@ -3,24 +3,24 @@ from aio_pika import Message, connect
 from config import settings
 import os
 
-class RabbitMQ:
-    def __init__(self):
-        self.address = (
-            f"amqp://admin:admin@rabbitmq/"
-        )
+# class RabbitMQ:
+#     def __init__(self):
+#         self.address = (
+#             f"amqp://admin:admin@rabbitmq/"
+#         )
 
-    async def __aenter__(self):
-        self.connection = await connect(self.address)
-        return self
+#     async def __aenter__(self):
+#         self.connection = await connect(self.address)
+#         return self
 
-    async def __aexit__(self, exc_type, exc, tb):
-        await self.connection.close()
+#     async def __aexit__(self, exc_type, exc, tb):
+#         await self.connection.close()
 
-    async def publish(self, message: str, routing_key: str):
-        channel = await self.connection.channel()
-        await channel.default_exchange.publish(
-            Message(message.encode("utf-8")), routing_key=routing_key
-        )
+#     async def publish(self, message: str, routing_key: str):
+#         channel = await self.connection.channel()
+#         await channel.default_exchange.publish(
+#             Message(message.encode("utf-8")), routing_key=routing_key
+#         )
 
 class RabbitMqMock:
         def __init__(self):
@@ -33,8 +33,9 @@ class RabbitMqMock:
             pass
 
 def get_rabbitmq():
-    if os.getenv("TESTING") == True:
-        return RabbitMqMock()
-    return RabbitMQ()
+    # if os.getenv("TESTING") == True:
+    #     return RabbitMqMock()
+    # return RabbitMQ()
+    return RabbitMqMock
 
 rabbit = get_rabbitmq()
