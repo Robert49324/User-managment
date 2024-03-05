7d033516e8eae43043d54bd39262090c1a1ad003
JustPC <robert49324@gmail.com>
2024-03-05 18:40:02 +0300

    ci/cd

--- src/config.py
+++ src/config.py
@@ -11,8 +11,8 @@ class Settings(BaseSettings):
     redis_url: str
 
     # rabbitmq
-    rabbitmq_user: str
-    rabbitmq_password: str
+    # rabbitmq_user: str
+    # rabbitmq_password: str
 
     # localstack
     localstack_url: str
@@ -35,8 +35,8 @@ class TestSettings(BaseSettings):
     redis_url: str
 
     # rabbitmq
-    rabbitmq_user: str
-    rabbitmq_password: str
+    # rabbitmq_user: str
+    # rabbitmq_password: str
 
     # localstack
     localstack_url: str
