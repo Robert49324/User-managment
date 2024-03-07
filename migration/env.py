d8fda8c390bb117d8a53e828e694350cbe6603b3
JustPC <robert49324@gmail.com>
2024-03-07 13:04:58 +0300

    group modul

--- migration/env.py
+++ migration/env.py
@@ -5,9 +5,11 @@ from alembic import context
 from sqlalchemy import pool
 from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import async_engine_from_config
+from src.config import settings
 
 from src.models import Base
 import os
+
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -29,7 +31,7 @@ target_metadata = Base.metadata
 # my_important_option = config.get_main_option("my_important_option")
 # ... etc.
 
-
+config.set_main_option('sqlalchemy.url', settings.postgres_url)
 def run_migrations_offline() -> None:
     """Run migrations in 'offline' mode.
 
@@ -42,11 +44,10 @@ def run_migrations_offline() -> None:
     script output.
 
     """
-    url = os.getenv("DATABASE_URL")
-    if url is None:
-        raise ValueError("DATABASE_URL environment variable not set")
+    # url = config.get_main_option("sqlalchemy.url")
+    # url = config.set_main_option('sqlalchemy.url', settings.postgres_url)
     context.configure(
-        url=url,
+        # url=url,
         target_metadata=target_metadata,
         literal_binds=True,
         dialect_opts={"paramstyle": "named"},
