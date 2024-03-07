9724e373c887383fb682d63d0d7290e59cdc77d8
JustPC <robert49324@gmail.com>
2024-03-07 16:04:30 +0300

    tests for group

--- tests/group/test_router.py
+++ tests/group/test_router.py
@@ -16,7 +16,7 @@ async def test_create_group(client):
 
 @pytest.mark.asyncio
 async def test_delete_group(client):
-    response = await client.post("/group/", json={"name": "new_group"})
+    response = await client.post("/group/create", json={"name": "new_group"})
     group_id = response.json()["id"]
     response = await client.delete(f"/group/{group_id}")
     assert response.status_code == 200
@@ -37,7 +37,7 @@ async def test_add_user_to_group(client):
     db = get_db()
     user = await db.execute(select(User).where(User.email == "email@email.com"))
     user_id = user.scalar().id
-    response = await client.post("/group/", json={"name": "one_more"})
+    response = await client.post("/group/create", json={"name": "one_more"})
     group_id = response.json()["id"]
     response = await client.post(f"/group/{group_id}/users/{user_id}")
     assert response.status_code == 200
