a7e685abbec45cbfff399f8efe3abcc93646269f
JustPC <robert49324@gmail.com>
2024-03-15 14:36:04 +0300

    clean architecture

--- tests/auth/test_router.py
+++ tests/auth/test_router.py
@@ -60,8 +60,7 @@ async def test_refresh_token(client):
     )
     access_token = login_response.json()["access_token"]
     refresh_token = login_response.json()["refresh_token"]
-    print(access_token)
-    headers = {"Authorization": f"Bearer {access_token}"}
+    headers = {"Authorization": f"Bearer {refresh_token}"}
     time.sleep(1)
     response = await client.post("/auth/refresh_token", headers=headers)
     print(response.json())
@@ -72,7 +71,7 @@ async def test_refresh_token(client):
 
 @pytest.mark.asyncio
 async def test_refresh_token_wrong_token(client):
-    response = await client.post("/auth/refresh_token", headers = {"Authorization": f"Bearer wrong_token"})
+    response = await client.post("/auth/refresh_token", json={"refresh_token": f"Bearer wrong_token"})
     assert response.status_code == 422
     assert response.json() == {"detail": "Could not validate the user."}
 
