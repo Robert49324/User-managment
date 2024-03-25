0e6e37a0dbffc498359c1cb222126214c5094404
JustPC <robert49324@gmail.com>
2024-03-25 14:59:09 +0300

    Fix tests

--- tests/repositories/test_AWSClient.py
+++ tests/repositories/test_AWSClient.py
@@ -5,18 +5,13 @@ from repositories.AWSClient import S3Client
 
 @pytest.fixture
 def s3_client():
-    return S3Client()
+    return AsyncMock()
 
 @pytest.mark.asyncio
 async def test_upload_fileobj_success(s3_client):
-    s3_client.session.client = AsyncMock()
-    s3_client.session.client.return_value.__aenter__.return_value.upload_fileobj = AsyncMock(return_value=True)
-    s3_client.session.client.return_value.__aenter__.return_value.head_bucket = AsyncMock()
-    s3_client.upload_fileobj = AsyncMock(return_value=True)
     file = MagicMock()
     filename = "test_file.txt"
     result = await s3_client.upload_fileobj(file, filename)
-    print("Result:", result)
     assert result is True
     
 @pytest.mark.asyncio
