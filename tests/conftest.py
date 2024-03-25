a80849a7ddbc85f0ffdf48c81b3b9543f3430eeb
JustPC <robert49324@gmail.com>
2024-03-25 09:24:09 +0300

    Fix tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -11,9 +11,9 @@ from repositories.RabbitClient import RabbitMQ
 from repositories.RedisClient import RedisClient
 from src.main import app
 
-app.dependency_overrides[RabbitMQ] = AsyncMock()
-app.dependency_overrides[S3Client] = AsyncMock()
-app.dependency_overrides[RedisClient] = AsyncMock()
+app.dependency_overrides[RabbitMQ] = lambda : AsyncMock()
+app.dependency_overrides[S3Client] = lambda : AsyncMock()
+app.dependency_overrides[RedisClient] = lambda : AsyncMock()
 
 
 @pytest.fixture(scope="session")
@@ -25,8 +25,8 @@ def event_loop() -> Generator[asyncio.AbstractEventLoop, None, None]:
 
 @pytest_asyncio.fixture
 async def client() -> AsyncGenerator[TestClient, None]:
-    # host, port = "127.0.0.1", "8000"
-    # scope = {"client": (host, port)}
+    host, port = "127.0.0.1", "8000"
+    scope = {"client": (host, port)}
 
-    async with TestClient(app) as client:
+    async with TestClient(app, scope=scope) as client:
         yield client
