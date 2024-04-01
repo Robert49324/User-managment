33aa9bab036fd869a83abea591c27399620c3601
JustPC <robert49324@gmail.com>
2024-04-01 10:33:11 +0300

    Clean architecture

--- tests/auth/test_router.py
+++ tests/auth/test_router.py
@@ -87,7 +87,7 @@ async def test_refresh_token(client):
 async def test_reset_password(client, mocker):
     mock_rabbitmq_enter = mocker.patch("repositories.RabbitClient.RabbitMQ.__aenter__", return_value=mocker.AsyncMock())
     mock_rabbitmq_exit = mocker.patch("repositories.RabbitClient.RabbitMQ.__aexit__", return_value=None)
-    mock_rabbitmq_publish = mocker.patch("repositories.RabbitClient.RabbitMQ.publish", new_callable=mocker.AsyncMock())
+    mock_rabbitmq_publish = mocker.patch("repositories.RabbitClient.RabbitMQ.publish", new_callable=mocker.AsyncMock)
 
     login_response = await client.post(
         "/auth/login", json={"email": "hT0Qf@example.com", "password": "password"}
