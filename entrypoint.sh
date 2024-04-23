13abb2b211b506df21befca54706ad73c5eb6640
JustPC <robert49324@gmail.com>
2024-04-23 10:33:58 +0300

    grpc starting

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-poetry run alembic upgrade head
-poetry run python src/main.py
+poetry run alembic upgrade head &
+poetry run python src/main.py &
 poetry run python src/grpc_server.py 
